fc582c5e287e5a69cdbbbe3afa3a713f4652e85a
appmonster007 <prathitlenovoa1000@gmail.com>
2021-01-11 20:43:13 +0530

    Added file via upload, being new to cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LHDCSHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var time = 2000;
+            Console.WriteLine("Its {0} ", time);
+        }
+    }
+}
